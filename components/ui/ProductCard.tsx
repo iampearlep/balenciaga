a5ebd6647a076374dd0569b5045ea3a988766f8e
iampearlep <damilolaprincess4@gmail.com>
2024-08-11 00:45:55 +0100

    feat: update components

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import ProductImg from "@/public/images/img3.jpg"
+import ProductImg from "@/public/images/pi4.jpg"
 
 export const ProductCard = () => {
   return (
     <div className="w-11/12 mx-auto">
       <div className="flex flex-col gap-y-3">
-       <div className="bg-white"> <Image src={ProductImg} height={634} width={751} alt=""/></div>
+       <div className="bg-white"> <Image src={ProductImg} height={1825} width={1178} alt=""/></div>
        <div className="flex flex-col gap-y-1">
         <h4>Outerwear Shirt Large Fit in Black</h4>
         <p>$1150</p>
