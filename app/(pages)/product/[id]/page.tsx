d0d64604eb433e2d9c9374fd160e7be1cd532a78
iampearlep <damilolaprincess4@gmail.com>
2024-10-23 22:48:08 +0100

    feat: add skeleton and update product details page

--- app/(pages)/product/[id]/page.tsx
+++ app/(pages)/product/[id]/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Product } from "@/types/product";
-
+import { ProductDetailsSkeleton } from "@/components/layouts/ProductDetailsSkeleton";
+import { CiCircleInfo } from "react-icons/ci";
 const fetchProduct = async (id: string): Promise<Product> => {
   const { data } = await axios.get(`/api/products/${id}`);
   return data;
@@ -19,13 +20,13 @@ export default function ProductPage({ params }: { params: { id: string } }) {
     queryFn: () => fetchProduct(params.id),
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <ProductDetailsSkeleton />;
   if (error) return <div>Error loading product</div>;
   if (!product) return <div>Product not found</div>;
 
   return (
     <div className="w-11/12 mx-auto py-32">
-      <div className="grid grid-cols-2 gap-x-14">
+      <div className="grid grid-cols-2 gap-x-10">
         <div>
           <Image
             src={`/images/${product.image}`}
@@ -36,23 +37,23 @@ export default function ProductPage({ params }: { params: { id: string } }) {
             className="w-full h-full"
           />
         </div>
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-semibold uppercase">{product.name}</h1>
-          <p className="py-2">{product.description}</p>
-          <p className="">{product.category}</p>
+        <div className="flex flex-col space-y-6">
+          <h1 className="text-3xl uppercase">{product.name}</h1>
+          <p className="">{product.description}</p>
+          <p className="">{product.subcategory}</p>
 
-          <div className="flex flex-row justify-between items-center py-5">
-            <p className="">${product.price}</p>
-            <p>Rating</p>
+          <div className="flex flex-row justify-between items-center">
+            <p className="font-semibold text-3xl">${product.price}</p>
+            <p>&#9733; &#9733; &#9733; &#9733; &#9734; (24)</p>
           </div>
-          <p>There are only a few left, order soon.</p>
-          <div className="py-6 ">
+          <p className="text-xs text-red-600 flex flex-row items-center space-x-2"><CiCircleInfo />There are only a few left. Order soon.</p>
+          <div className="">
             <p>Size:</p>
-           <div className="flex flex-row justify-between w-5/12"> {product.size.map((size, index) => {
-              return <button className="px-4 py-2 rounded-sm hover:border-gray-950 hover:border " key={index}>{size}</button>;
+           <div className="flex flex-row space-x-5 py-2"> {product.size.map((size, index) => {
+              return <button className="px-3 py-1 rounded-sm border border-gray-950 hover:text-white hover:bg-black transition-all duration-300 ease-linear" key={index}>{size}</button>;
             })}</div>
           </div>
-          <div className="flex flex-col gap-6 ">
+          <div className="flex flex-col gap-6">
             <button className="bg-black rounded text-white py-2">Add to bag</button>
             <button className="bg-white border-black rounded-sm py-2">Buy now</button>
           </div>
---
+++ components/layouts/ProductDetailsSkeleton.tsx
@@ -0,0 +1,43 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
+
+export const ProductDetailsSkeleton = () => {
+  return (
+   <div className="w-11/12 mx-auto py-32">
+     <div className="grid grid-cols-2 gap-x-10">
+    <div>
+        <Skeleton className="h-[600px] w-full rounded-sm" />
+    </div>
+    <div className="flex flex-col gap-y-4">
+        <div>
+        <Skeleton className="h-[50px] w-[400px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[30px] w-[300px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[15px] w-[50px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[20px] w-full rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[15px] w-[200px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[20px] w-[200px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[50px] w-[250px] rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[40px] w-full rounded-sm" />
+        </div>
+        <div>
+        <Skeleton className="h-[40px] w-full rounded-sm" />
+        </div>
+    </div>
+  </div>
+   </div>
+  )
+}
