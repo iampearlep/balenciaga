d565495e437d021ecb7a1a0aacf2bdfd388d7642
iampearlep <damilolaprincess4@gmail.com>
2024-12-26 14:15:12 +0100

    feat: fix payment method, refactor CheckoutSummary and checkout page for improved readability and structure

--- app/(pages)/checkout/page.tsx
+++ app/(pages)/checkout/page.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use client";
 import CheckoutCartSummary from "@/components/CheckoutCartSummary";
 import CheckoutInfo from "@/components/CheckoutInfo";
 
 export default function Page() {
-    return (
-        <main>
-       <div className="mx-auto bg-white">
+  return (
+    <main>
+      <div className="mx-auto bg-white">
         <div className="bg-[#f7f7f7] py-8">
-            <h1 className="w-11/12 mx-auto uppercase text-3xl md:text-[2.8rem]">Checkout</h1>
+          <h1 className="w-11/12 mx-auto uppercase text-3xl md:text-[2.8rem]">
+            Checkout
+          </h1>
         </div>
         <div className="w-11/12 mx-auto flex flex-col-reverse md:flex-row gap-y-6 gap-x-28 py-10">
-            <div className="md:w-8/12">
-                <CheckoutInfo />
-            </div>
-            <div className="md:w-4/12">
-                <CheckoutCartSummary />
-            </div>
+          <div className="md:w-8/12">
+            <CheckoutInfo />
+          </div>
+          <div className="md:w-4/12">
+            <CheckoutCartSummary />
+          </div>
         </div>
-       </div>
-        </main>
-    );
+      </div>
+    </main>
+  );
 }
--- components/CheckoutInfo.tsx
+++ components/CheckoutInfo.tsx
@@ -1,72 +1,208 @@
-"use client"
-import Link from "next/link"
-import Contact from "./Contact"
-import Delivery from "./Delivery"
-import { Button } from "./ui/button"
-import { usePaystackPayment } from 'react-paystack'
-import { useRouter } from "next/navigation"
-import useCartStore from "@/store/cartStore"
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import useCartStore from "@/store/cartStore";
+import { PaystackButton } from "react-paystack";
 
 const CheckoutInfo = () => {
-    const {clearCart, getTotalPrice} =  useCartStore()
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [surname, setSurName] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [deliverycity, setDeliveryCity] = useState<string>("");
+  const [deliveryphone, setDeliveryPhone] = useState<string>("");
+  const publicKey = process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY || "";
+  const { clearCart, getTotalPrice } = useCartStore();
 
-    const router = useRouter()
+  const billings = {
+    discount: 0.9,
+    total: getTotalPrice(),
+  };
 
-    const total = () => {
-        return getTotalPrice()
-      }; 
+  const amount = Math.round(billings.total * 1500 * billings.discount * 100);
 
-    const billings = {
-        discount: 0.9,
-        total: total 
-    }
+  const router = useRouter();
 
-    const config = {
-        reference: (new Date()).getTime().toString(),
-        email: "damilolaprincess4@gmail.com",
-        amount: Math.round(billings.total() * 1500 * billings.discount * 100),
-        publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY || '',
-    }
-  
-    const onSuccess = (reference: any) => {
-        clearCart()
-        router.push('/success')
-    }
+  const componentProps = {
+    email,
+    amount,
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Name",
+          variable_name: "name",
+          value: name,
+        },
+        {
+          display_name: "SurName",
+          variable_name: "surname",
+          value: surname,
+        },
+        {
+          display_name: "Phone Number",
+          variable_name: "phone_number",
+          value: phone,
+        },
+        {
+          display_name: "City",
+          variable_name: "city",
+          value: city,
+        },
+        {
+          display_name: "Address",
+          variable_name: "address",
+          value: address,
+        },
+        {
+          display_name: "Delivery Phone Number",
+          variable_name: "deliveryphone",
+          value: deliveryphone,
+        },
+        {
+          display_name: "Delivery City",
+          variable_name: "delivery-city",
+          value: deliverycity,
+        },
+      ],
+    },
+    publicKey,
+    text: "CONTINUE TO PAYMENT",
+    onSuccess: () => {
+      clearCart(), router.push("/success");
+    },
+    onClose: () => alert("Wait! don't go :("),
+  };
 
-    const onClose = () => {
-        alert('Wait, Dont leave :(')
-    }
-
-    const initializePayment = usePaystackPayment(config)
-
-    return (
-        <div>
-            <Contact />
-            <Delivery />
-            <div className="flex justify-between py-20">
-                <Link href='/cart'>
-                    <Button 
-                        size='default' 
-                        className="uppercase rounded-sm bg-white text-black hover:text-white border border-black"
-                    >
-                        Go Back
-                    </Button>
-                </Link>
-                <Button 
-                    size='default' 
-                    className="uppercase rounded-sm"
-                    onClick={() => {
-                        initializePayment({
-                            onSuccess,
-                            onClose,
-                        })
-                    }}
-                >
-                    Continue to payment
-                </Button>
+  return (
+    <div>
+      <div>
+        <h3 className="uppercase text-[1.65rem] py-6">Contact information</h3>
+        <div className="grid grid-cols-2 gap-x-4 pb-2">
+          <div>
+            <Label htmlFor="name" className="text-sm text-slate-400">
+              Name
+            </Label>
+            <Input
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-x-4">
+            <div>
+              <Label htmlFor="surname" className="text-sm text-slate-400">
+                Surname*
+              </Label>
+              <Input
+                id="surname"
+                value={surname}
+                onChange={(e) => setSurName(e.target.value)}
+                className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+              />
             </div>
+            <div>
+              <Label htmlFor="city" className="text-sm text-slate-400">
+                City*
+              </Label>
+              <Input
+                id="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="grid grid-cols-2 gap-x-4">
+          <div>
+            <Label htmlFor="phone-number" className="text-sm text-slate-400">
+              Phone Number*
+            </Label>
+            <Input
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+            />
+          </div>
+          <div>
+            <Label htmlFor="email" className="text-sm text-slate-400">
+              Email
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3 className="uppercase text-[1.65rem] py-6">Delivery information</h3>
+        <div className="grid grid-cols-2 gap-x-4 pb-2">
+          <div>
+            <Label htmlFor="address" className="text-sm text-slate-400">
+              Address
+            </Label>
+            <Input
+              id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+            />
+          </div>
+          <div>
+            <Label htmlFor="delivery-city" className="text-sm text-slate-400">
+              City*
+            </Label>
+            <Input
+              id="delivery-city"
+              value={deliverycity}
+              onChange={(e) => setDeliveryCity(e.target.value)}
+              className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+            />
+          </div>
+        </div>
+        <div>
+          <Label
+            htmlFor="delivery-phone-number"
+            className="text-sm text-slate-400"
+          >
+            Phone Number*
+          </Label>
+          <Input
+            id="delivery-phone"
+            value={deliveryphone}
+            onChange={(e) => setDeliveryPhone(e.target.value)}
+            className="h-5 border-t-0 border-x-0 shadow-none border-black rounded-none px-1 focus-visible:ring-0"
+          />
+        </div>
+      </div>
+      <div className="flex justify-between py-20">
+        <Link href="/cart">
+          <Button
+            size="default"
+            className="uppercase rounded-sm bg-white text-black hover:text-white border border-black"
+          >
+            Go Back
+          </Button>
+        </Link>
+        <PaystackButton
+          className="text-white bg-black border-black border rounded-sm h-10 px-8"
+          {...componentProps}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default CheckoutInfo
+export default CheckoutInfo;
