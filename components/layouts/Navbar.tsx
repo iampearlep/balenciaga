5c588bace4700ad3ea5f09c8bb300611d36d1c06
iampearlep <damilolaprincess4@gmail.com>
2024-12-27 14:25:00 +0100

    feat: implement mobile nav animations using framer-motion; refactor Navbar component for improved state management

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineUser } from "react-icons/ai";
 import { IoMdHeartEmpty, IoIosSearch, IoIosLogOut } from "react-icons/io";
-import { HiOutlineShoppingBag } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "next/navigation";
 import { CartDrawer } from "../CartDrawer";
+import { AnimatePresence, motion } from 'framer-motion'
 const Navbar = () => {
-  const [isToggle, setIsToggle] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const { user, logOut } = useAuthStore((state) => ({
     user: state.user,
@@ -18,8 +18,8 @@ const Navbar = () => {
 
   const router = useRouter();
 
-  const handleToggle = () => {
-    setIsToggle(!isToggle);
+  const toggleMenu = () => {
+    setOpen((prevOpen) => !prevOpen);
   };
 
   const handleLogOut = async () => {
@@ -35,6 +35,70 @@ const Navbar = () => {
       return user.email.charAt(0).toUpperCase();
     }
     return <AiOutlineUser className="w-5 h-4" />;
+  };
+
+
+  const mobileNavLinks = [
+    { title: "Home", href: "/" },
+    { title: "Men", href: "/men" },
+    { title: "Women", href: "/women" },
+    { title: "Kids", href: "/" },
+    { title: "Explore", href: "/explore" },
+    { title: "New", href: "/new" },
+  ];
+
+
+  const menuVars = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+        ease: [0.12, 0, 0.39, 0],
+      }
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        duration: 0.5,
+        ease: [0.22, 1, 0.36, 1],
+      }
+    }
+  }
+
+  const mobileLinkVars = {
+    initial: {
+      y: "30vh",
+      transition: {
+        duration: 0.5,
+        ease: [0.37, 0, 0.63, 1],
+      }
+    },
+    open: {
+      y: 0,
+      transition: {
+        duration: 0.7,
+        ease: [0, 0.55, 0.45, 1],
+      }
+    }
+  }
+
+  const containerVars = {
+    initial: {
+      transition: {
+        staggerChildren: 0.09,
+        staggerDirection: -1,
+      },
+    },
+    open: {
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.09,
+        staggerDirection: 1,
+      },
+    },
   };
 
   return (
@@ -96,8 +160,8 @@ const Navbar = () => {
       <div>
         <div className="flex md:hidden flex-row justify-between items-center py-6">
           <div className="flex flex-row gap-x-3 items-center justify-center font-medium">
-            <button onClick={handleToggle}>
-              {isToggle ? (
+            <button onClick={toggleMenu}>
+              {open ? (
                 <AiOutlineClose className="w-5 h-4" />
               ) : (
                 <SlMenu className="w-5 h-4" />
@@ -135,69 +199,46 @@ const Navbar = () => {
             <CartDrawer />
           </div>
         </div>
-
-        {isToggle && (
-          <div className="fixed top-0 right-0 h-full  w-full bg-[#bebebe] z-30 flex flex-col justify-start items-start">
-            <div className="flex flex-row justify-between items-center ">
-              <h1
-                className={`absolute top-7 left-5 text-xl font-semibold uppercase tracking-widest`}
-              >
-                <Link href="/" className="cursor-pointer">
-                  Balenciaga
-                </Link>
-              </h1>
-              <button onClick={handleToggle} className="absolute top-9 right-5">
-                <AiOutlineClose className="w-5 h-4" />
-              </button>
-            </div>
-            <div className="mt-40 max-h-screen overflow-y-auto w-full">
-              <div className="flex flex-col gap-7 items-start justify-start font-semibold text-xl px-5 ">
-                <Link
-                  href="/"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
+        <AnimatePresence>
+        {open && (
+          <motion.div
+            variants={menuVars}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="fixed left-0 top-0 w-full h-full z-30 origin-top bg-black text-white p-10"
+          >
+            <div className="flex h-full flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-xl uppercase">Balenciaga</h1>
+                <p
+                  className="cursor-pointer"
+                  onClick={toggleMenu}
                 >
-                  Home
-                </Link>
-                <Link
-                  href="/men"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
-                >
-                  Men
-                </Link>
-                <Link
-                  href="/women"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
-                >
-                  Women
-                </Link>
-                <Link
-                  href="/kids"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
-                >
-                  Kids
-                </Link>
-                <Link
-                  href="/explore"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
-                >
-                  Explore
-                </Link>
-                <Link
-                  href="/new"
-                  onClick={handleToggle}
-                  className="cursor-pointer w-full py-2"
-                >
-                  New
-                </Link>
+                  <AiOutlineClose className="w-5 h-4" />
+                </p>
               </div>
+              <motion.div
+                variants={containerVars}
+                initial="initial"
+                animate="open"
+                exit="initial"
+                className="flex flex-col h-full justify-center font-lora items-center gap-4 "
+              >
+                {mobileNavLinks.map((link, index) => {
+                  return (
+                    <div className="overflow-hidden">
+                      <motion.div key={index} variants={mobileLinkVars} className="uppercase text-2xl">
+                        <Link href={link.href} onClick={toggleMenu}>{link.title}</Link>
+                      </motion.div>
+                    </div>
+                  );
+                })}
+              </motion.div>
             </div>
-          </div>
+          </motion.div>
         )}
+      </AnimatePresence>
       </div>
     </div>
   );
